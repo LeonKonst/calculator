d5247fe54dc025ee838bda359b3c7c13a1966aa8
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-09-09 16:29:14 +0200

    Add a function to populate the first number and display the number when digits are clicked

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function multiply(a, b){
 function divide(a, b){
     return b!==0?a/b:Infinity;
 }
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+function operate(a, b, operator){
+    return operator(a,b);
+}
+
+
+const digitBtns = document.querySelector("#digit-buttons");
+const display = document.querySelector("#display");
+
+
+
+digitBtns.addEventListener("click", (e) => {
+    if(e.target.tagName==="BUTTON"){
+        display.textContent += e.target.value;
+        firstNumber += e.target.value;
+        
+    }
+}
+);
